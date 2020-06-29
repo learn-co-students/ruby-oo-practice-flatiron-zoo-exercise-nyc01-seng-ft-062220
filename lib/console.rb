60bc5ffc1658bd93fdba4ffbdd94053f5e7c218d
Luis Reyes <luisreyesxv@yahoo.com>
2020-06-29 11:21:48 -0400

    Done.

---
+++ lib/console.rb
@@ -0,0 +1,24 @@
+require_relative './Animal.rb'
+require_relative './Zoo.rb'
+require 'pry'
+
+bronx_zoo = Zoo.new("Bronx Zoo", "Bronx,NY")
+brooklyn_zoo = Zoo.new("Brooklyn Zoo", "Brooklyn,NY")
+swag_zoo = Zoo.new("Party in the Zoo-S-A", "Party Central")
+wwe_zoo = Zoo.new("WwE Zoo", "Party Central")
+
+
+
+
+panda_1 = Animal.new("Tucker","panda",400) #bronx
+panda_2 = Animal.new("Avenger","panda",347) #brooklyn
+panda_3 = Animal.new("Courage", "panda",275) #bronx
+dog_1 = Animal.new("April","dog",50) #wwe
+dog_2 = Animal.new("Mio","dog",35) #wwe
+zebra_1 = Animal.new("Craig","zebra", 300) #swag
+iguana_1 = Animal.new("Dead-eye","iguana", 45) #brooklyn
+
+
+
+
+
