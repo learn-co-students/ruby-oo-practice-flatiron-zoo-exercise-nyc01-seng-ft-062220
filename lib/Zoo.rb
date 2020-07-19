7796e6ce2d68fcd73239f27a3ca0d186fa6100ee
Alex <alex.beciana@gmail.com>
2020-07-18 20:15:05 -0400

    Done.

--- lib/Zoo.rb
+++ lib/Zoo.rb
@@ -1,3 +1,40 @@
 class Zoo
 
+    attr_accessor :name, :location
+
+    @@all = []
+
+    def initialize(name, location)
+        @name = name
+        @location = location
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def animals
+        Animal.all.select { |animal| animal.zoo == self }
+    end
+
+    def animal_species
+        animals.collect { |animal| animal.species }        
+    end
+
+    def find_by_species(species)
+        animals.select { |animal| animal.species == species }
+    end
+
+    def animal_nicknames
+        animals.collect do |a| 
+            a.nickname 
+            # binding.pry
+        end
+    end
+
+    def self.find_location(location)
+        self.all.select { |zoo| zoo.location == location}
+    end
+
 end
--- run.rb
+++ run.rb
@@ -4,7 +4,28 @@ require 'pry'
 
 
 #Test your code here
+z1 = Zoo.new("Bronx Zoo", "Bronx, NY")
 
+a1 = Animal.new("Snake", 5, "Bob", z1)
+a2 = Animal.new("Snake", 10, "Robert", z1)
+
+Animal.all
+
+a1.zoo
+
+Animal.find_by_species("Snake")
+
+z1.animals
+
+z1.animal_species
+
+z1.find_by_species("Snake")
+
+Animal.find_by_species("Snake")
+
+z1.animal_nicknames
+
+Zoo.find_location("Bronx, NY")
 
 binding.pry
 puts "done"
